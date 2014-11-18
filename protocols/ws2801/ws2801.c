9566ae84eede7628af6418707ef0817c8efe2b42
macmattes <mattes.b@gmx.net>
2014-11-18 14:15:26 +0100

    hsv to rgb conversion added

--- protocols/ws2801/ws2801.c
+++ protocols/ws2801/ws2801.c
@@ -360,6 +360,37 @@ void ws2801_setColorTemp(uint16_t k)
     ws2801_setColor(Red,Green,Blue);
 }
 
+void hsv_to_rgb(unsigned char h, unsigned char s, unsigned char v)
+ {
+ unsigned char r,g,b, i, f;
+ unsigned int p, q, t;
+ 
+if( s == 0 )
+   { r = g = b = v;
+ }
+ else
+ { i=h/43;
+ f=h%43;
+ p = (v * (255 - s))/256;
+ q = (v * ((10710 - (s * f))/42))/256;
+ t = (v * ((10710 - (s * (42 - f)))/42))/256;
+ 
+switch( i )
+ 	{ case 0:
+ 	r = v; g = t; b = p; break;
+ 	case 1:
+ 	r = q; g = v; b = p; break;
+ 	case 2:
+ 	r = p; g = v; b = t; break;
+ 	case 3:
+ 	r = p; g = q; b = v; break;
+ 	case 4:
+ 	r = t; g = p; b = v; break;
+ 	case 5:
+ 	r = v; g = p; b = q; break;
+ 	}
+ }
+
 //Datenausgabe
 
 void ws2801_show_storage(void)
